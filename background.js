7743b1e5c7e5543c1d53cf7a69354d1e1950bcad
egg-beetle <122716772+egg-beetle@users.noreply.github.com>
2023-03-19 20:40:48 +0800

    Update background.js

--- background.js
+++ background.js
@@ -1,1 +1,19 @@
-console.log("from background.js")
+console.log("from background.js");
+chrome.contextMenus.create(
+  
+  {type:"normal",
+   title:"鼠标右键、选中出现的标题“,
+   contexts:["selection","link"],
+   onclick:function(info)
+	   {console.log(info)}},
+  
+  function(){})
+chrome.contextMenus.create(
+  
+  {type:"checkbox",
+   title:"checkbox标题”，
+   contexts:["selection","link"],
+   onclick:function(info)
+	   {consle.log(info)}
+	},
+  function(){})
